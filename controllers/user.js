1c622518f59d639318015dc3596d90faaf39c1d1
Daniel Silva <daniel.silva@wikki.com.br>
2022-11-20 01:38:01 -0300

    Adicionados logs

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,15 @@ const User = require("../models/user");
 exports.createUser = async (req, res) => {
   const { fullname, username, email, password } = req.body;
   const isNewUser = await User.findOne({ email });
-  if (isNewUser)
+  if (isNewUser) {
+    console.log(`Email já cadastrado (${email})`);
     return res.json({
       success: false,
       message: "Este email já está em uso, tente fazer login",
     });
+  }
 
+  console.log(`Novo usuário criado (${email})`);
   const user = await User({
     username,
     fullname,
@@ -25,24 +28,27 @@ exports.userSignIn = async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (!user)
+  if (!user) {
+    console.log(`Usuário não encontrado (${email})`);
     return res.json({
       success: false,
       message: "Não foi encontrado um usuário com este email",
     });
-
-  if (user.failedLoginAttempts >= 3)
+  }
+  if (user.failedLoginAttempts >= 3) {
+    console.log(`Limite de tentativas de acesso excedido (${email})`);
     return res.json({
       success: false,
       message: "Limite de tentativas excedido, tente recuperar a senha",
     });
-
+  }
   const isMatch = user.password === password;
   if (!isMatch) {
     await User.findOneAndUpdate(
       { email },
       { failedLoginAttempts: user.failedLoginAttempts + 1 }
     );
+    console.log(`Email ou senha inválidos (${email})`);
     return res.json({
       success: false,
       message: "email ou senha inválidos",
@@ -57,5 +63,6 @@ exports.userSignIn = async (req, res) => {
     email: user.email,
   };
 
+  console.log(`Usuário autorizado (${email})`);
   res.json({ success: true, user: userInfo });
 };
