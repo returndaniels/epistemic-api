518598ed96d4516983a30b19c3e79c17a4da74ab
Daniel Silva <daniel.silva@wikki.com.br>
2022-11-20 03:55:53 -0300

    Ajuste na validação

--- controllers/user.js
+++ controllers/user.js
@@ -25,48 +25,50 @@ exports.createUser = async (req, res) => {
 
 exports.userSignIn = async (req, res) => {
   const { email, password } = req.body;
-  let user = null;
   try {
-    user = await User.findOne({ email });
-  } catch (error) {
-    console.log("Falha ao buscar usuário.");
-  }
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    console.log(`Usuário não encontrado (${email})`);
+    if (!user) {
+      console.log(`Usuário não encontrado (${email})`);
+      return res.json({
+        success: false,
+        message: "Não foi encontrado um usuário com este email",
+      });
+    }
+    if (user.failedLoginAttempts >= 3) {
+      console.log(`Limite de tentativas de acesso excedido (${email})`);
+      return res.json({
+        success: false,
+        message: "Limite de tentativas excedido, tente recuperar a senha",
+      });
+    }
+    const isMatch = user.password === password;
+    if (!isMatch) {
+      await User.findOneAndUpdate(
+        { email },
+        { failedLoginAttempts: user.failedLoginAttempts + 1 }
+      );
+      console.log(`Email ou senha inválidos (${email})`);
+      return res.json({
+        success: false,
+        message: "email ou senha inválidos",
+      });
+    }
+
+    await User.findOneAndUpdate({ email }, { failedLoginAttempts: 0 });
+
+    const userInfo = {
+      username: user.username,
+      fullname: user.fullname,
+      email: user.email,
+    };
+
+    console.log(`Usuário autorizado (${email})`);
+    res.json({ success: true, user: userInfo });
+  } catch (error) {
     return res.json({
       success: false,
       message: "Não foi encontrado um usuário com este email",
     });
   }
-  if (user.failedLoginAttempts >= 3) {
-    console.log(`Limite de tentativas de acesso excedido (${email})`);
-    return res.json({
-      success: false,
-      message: "Limite de tentativas excedido, tente recuperar a senha",
-    });
-  }
-  const isMatch = user.password === password;
-  if (!isMatch) {
-    await User.findOneAndUpdate(
-      { email },
-      { failedLoginAttempts: user.failedLoginAttempts + 1 }
-    );
-    console.log(`Email ou senha inválidos (${email})`);
-    return res.json({
-      success: false,
-      message: "email ou senha inválidos",
-    });
-  }
-
-  await User.findOneAndUpdate({ email }, { failedLoginAttempts: 0 });
-
-  const userInfo = {
-    username: user.username,
-    fullname: user.fullname,
-    email: user.email,
-  };
-
-  console.log(`Usuário autorizado (${email})`);
-  res.json({ success: true, user: userInfo });
 };
